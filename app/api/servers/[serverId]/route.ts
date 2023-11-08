55ee9d0fdcc5df73a64167eabdd302eed93c2646
poggy33 <33poggy33@gmail.com>
2023-11-08 10:48:27 +0000

    all done to manage server and settings

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -1,39 +1,59 @@
-// import {v4 as uuidv4} from "uuid";
-// import { currentProfile } from "@/lib/current-profile";
-// import { db } from "@/lib/db";
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-// export async function PATCH (
-//     req: Request,
-//     {params}: {params: {serverId: string}}
-// ) {
-// try {
-// const profile = await currentProfile();
-// const { name, imageUrl} = await req.json();
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
 
-// if(!profile) {
-//     return new NextResponse("Unauthorized", {status: 401})
-// }
+export async function DELETE(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
 
-// if(!params.serverId) {
-//     return new NextResponse("Server Id Missing", {status: 400})
-// }
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
 
-// const server = await db.server.update({
-//     where: {
-//         id: params.serverId,
-//         profileId: profile.id,
-//     },
-//     data: {
-//         inviteCode: uuidv4(),
-//     },
-// });
+    const server = await db.server.delete({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      }
+    });
 
-// return NextResponse.json(server);
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_ID_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
 
-// } catch (error) {
-//     console.log(["SERVER_ID_PATCH"], error);
-//     return new NextResponse("Internal Error", {status: 500})
-// };
+export async function PATCH(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    const { name, imageUrl } = await req.json();
 
-// } 
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+      data: {
+        name,
+        imageUrl,
+      }
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_ID_PATCH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
