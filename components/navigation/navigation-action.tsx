e0879825d428fc74daf57e1822af91eea6f18a4e
poggy33 <33poggy33@gmail.com>
2023-11-12 11:12:05 +0000

    all done before start build components for chat!!!

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -18,8 +18,8 @@ export const NavigationAction = () => {
       <button onClick={() => onOpen("createServer")} className=" group flex items-center">
         <div
           className="flex mx-3 h-[48px] w-[48px] rounded-[24px] 
-                group-hover:rounded-[16px] transition-all overflow-hidden
-                 items-center justify-center bg-bakground dark:bg-neutral-700
+                 transition-all overflow-hidden
+                 items-center justify-center bg-white dark:bg-neutral-700
                  group-hover:bg-emerald-500"
         >
           <Plus
