f112f9cd75a8e92ea11d9edb4c671dda0cacd07a
poggy33 <33poggy33@gmail.com>
2023-11-15 14:27:15 +0000

    before changing version next and before change config file

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,6 +1,6 @@
 "use client";
-import {X} from "lucide-react"
-import Image from "next/image"
+import { FileIcon, X } from "lucide-react";
+import Image from "next/image";
 
 import { UploadDropzone } from "@/lib/uploadthing";
 
@@ -13,24 +13,42 @@ interface FileUploadProps {
 }
 
 export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
-    const fileType = value?.split(".").pop();
-    if(value && fileType !== "pdf") {
-        return (
-            <div className="relative h-20 w-20">
-                <Image
-                fill
-                src={value}
-                alt="Upload"
-                className="rounded-full"
-                />
-                <button onClick={() => onChange("")} type="button" className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm">
-                    <X className="h-4 w-4"/>
-                </button>
+  const fileType = value?.split(".").pop();
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image fill src={value} alt="Upload" className="rounded-full" />
+        <button
+          onClick={() => onChange("")}
+          type="button"
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+  if (value && fileType === "pdf") {
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+        <a
+          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+          href={value}
+          target="_blank"
+          rel="noopener noreferer"
+        >{value}</a>
+        <button
+          onClick={() => onChange("")}
+          type="button"
+          className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
 
-            </div>
-        )
-    }
-    
   return (
     <UploadDropzone
       endpoint={endpoint}
@@ -38,7 +56,7 @@ export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
         onChange(res?.[0].url);
       }}
       onUploadError={(error: Error) => {
-        console.log(error)
+        console.log(error);
       }}
     />
   );
